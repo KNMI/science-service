9d19bb00a51c423075fbfd560e80b8ede36d5df2
andrejsim <andrej@knmi.nl>
2018-01-15 12:30:01 +0100

    exec.

--- exec-docker.sh
+++ exec-docker.sh
@@ -4,5 +4,6 @@ echo "execute cwl workflow in"
 echo "container: " ${1}
 echo "input.yml:" ${2}
 
+docker ps
 docker cp ${2} ${1}:/home/jovyan/${2}
 docker exec -it ${1} bash -c "cwl-runner science-service/workflows/epos_accept_url_array_scatter.cwl ${2}"
